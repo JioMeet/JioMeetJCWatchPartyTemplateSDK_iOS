a36bf46575e89d8cc5b92fd4f94a2157abd5021d
Rohit <rohit.kumar@bigsteptech.com>
2024-03-27 12:50:31 +0530

    Updated Package swift file for SPM Demo

--- Package.swift
+++ Package.swift
@@ -15,9 +15,29 @@ let package = Package(
 	],
 	dependencies: [
 		.package(
-			name: "JioMeetCoreSDK",
-			url: "https://github.com/JioMeet/JioMeetCoreSDK_iOS.git",
-			.upToNextMajor(from: "3.0.0-alpha.1")
+            name: "SwiftyJSON",
+            url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
+            .upToNextMajor(from: "5.0.1")
+        ),
+		.package(
+            name: "SocketIO",
+            url: "https://github.com/socketio/socket.io-client-swift.git", 
+           .upToNextMajor(from: "16.1.0")
+        ),
+		.package(
+            name: "MMWormhole",
+            url: "https://github.com/JioMeet/MMWormhole.git",
+            .upToNextMajor(from: "2.1.0")
+        ),
+        .package(
+            name: "VoiceActivityDetector",
+            url: "https://github.com/JioMeet/WebRtcVad.git",
+            .exact("1.0.0")
+        ),
+		.package(
+			name: "JioMeetRtcEngine_iOS",
+			url: "https://github.com/JioMeet/JioMeet-rtc-engine-ios.git",
+			.exact("3.7.2")
 		),
 		.package(
 			name: "Lottie",
@@ -32,17 +52,34 @@ let package = Package(
 	],
 	targets: [
 		.binaryTarget(
+			name: "JMMediaStackSDK",
+			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/spm_demo_2/JMMediaStackSDK.xcframework.zip",
+			checksum: "f12392b08615e7716aca762c60b707594b0e65ddc3ae46d72f97537e590c94ea"
+		),
+		.binaryTarget(
+			name: "JioMeetCoreSDK",
+			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/spm_demo_2/JioMeetCoreSDK.xcframework.zip",
+			checksum: "cc37a8717dab972327ccf5b0b5532c70b400b8ec1a2e0df6975d94d29866b669"
+		),
+		.binaryTarget(
 			name: "JioMeetWatchParty",
-			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/v_2_0_0_alpha_3/JioMeetWatchParty.xcframework.zip",
-			checksum: "e4d80e75ed7e2bc011b778f8a24dce333fc5120f140e6f272ead815a3aec5b1e"
+			url: "https://storage.googleapis.com/cpass-sdk/libs/iOS/internal/jv_watch_party/spm_demo_2/JioMeetWatchParty.xcframework.zip",
+			checksum: "75c5831e08862ac5c8e93145c5fa6f59e54251cf7d1206f6d8958db9e5aecfa6"
 		),
 		.target(
 			name: "JioMeetWatchPartyTarget",
 			dependencies: [
+				.target(name: "JMMediaStackSDK"),
+				.target(name: "JioMeetCoreSDK"),
 				.target(name: "JioMeetWatchParty"),
+				.product(name: "SwiftyJSON", package: "SwiftyJSON"),
+                .product(name: "SocketIO", package: "SocketIO"),
+                .product(name: "MMWormhole", package: "MMWormhole"),
+                .product(name: "VoiceActivityDetector", package: "VoiceActivityDetector"),
+				.product(name: "RTC", package: "JioMeetRtcEngine_iOS"),
+				.product(name: "RTM", package: "JioMeetRtcEngine_iOS"),
 				.product(name: "Lottie", package: "Lottie"),
-				.product(name: "FLAnimatedImage", package: "FLAnimatedImage"),
-				.product(name: "JioMeetCoreSDK", package: "JioMeetCoreSDK")
+				.product(name: "FLAnimatedImage", package: "FLAnimatedImage")
 			],
 			path: "Framework/Dependency",
 			exclude: []
