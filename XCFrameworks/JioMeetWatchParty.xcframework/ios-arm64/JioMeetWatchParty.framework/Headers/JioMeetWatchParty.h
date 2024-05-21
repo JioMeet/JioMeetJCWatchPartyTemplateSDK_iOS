c6bbdbb9649601b7c05759d8ebfc5e5361dc5b3c
Rohit <rohit.kumar@bigsteptech.com>
2024-05-21 23:07:02 +0530

    Testing SPM Release 3 Series

---
+++ XCFrameworks/JioMeetWatchParty.xcframework/ios-arm64/JioMeetWatchParty.framework/Headers/JioMeetWatchParty.h
@@ -0,0 +1,18 @@
+//
+//  JioMeetWatchParty.h
+//  JioMeetWatchParty
+//
+//  Created by Rohit41.Kumar on 15/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JioMeetWatchParty.
+FOUNDATION_EXPORT double JioMeetWatchPartyVersionNumber;
+
+//! Project version string for JioMeetWatchParty.
+FOUNDATION_EXPORT const unsigned char JioMeetWatchPartyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JioMeetWatchParty/PublicHeader.h>
+
+
